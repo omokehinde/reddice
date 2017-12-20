e21b316a5aa46cf54f87f4e4cfbec1964935df1b
omokehinde <engr.omokehinde@gmail.com>
2017-12-20 01:39:04 +0100

    Hot reloading setup

--- server/index.js
+++ server/index.js
@@ -3,11 +3,19 @@ import path from 'path';
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev';
 
 let app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
 // app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 
 app.get('/*', (req, res) => {
